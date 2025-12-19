78eeaac888848c90a4b3dd1da839268966e120d9
Masahiro Iuchi <miuchi@sixapart.com>
2025-12-19 19:02:26 +0900

    Limit eslint scope to src directory

--- mt-plugin-content-field-type-email-react/eslint.config.js
+++ mt-plugin-content-field-type-email-react/eslint.config.js
@@ -4,7 +4,7 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["src/**/*.{js,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
   {
     settings: {
