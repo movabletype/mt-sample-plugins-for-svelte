3b2dc67d23fa674adcd27c25d33ebad314e72041
Masahiro Iuchi <miuchi@sixapart.com>
2025-12-19 23:01:39 +0900

    Use eslint-plugin-react-hooks

--- mt-plugin-content-field-type-email-react/eslint.config.js
+++ mt-plugin-content-field-type-email-react/eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig } from "eslint/config";
 
@@ -12,6 +13,7 @@ export default defineConfig([
     languageOptions: { globals: globals.browser }
   },
   pluginReact.configs.flat.recommended,
+  pluginReactHooks.configs.flat.recommended,
   pluginReactRefresh.configs.recommended,
   {
     settings: {
