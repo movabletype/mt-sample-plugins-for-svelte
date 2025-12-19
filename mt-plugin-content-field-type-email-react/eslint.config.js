bd99c6dfed40175776e322d5657048787ce2c14e
Masahiro Iuchi <miuchi@sixapart.com>
2025-12-19 19:00:45 +0900

    Add react version detection to eslint config

--- mt-plugin-content-field-type-email-react/eslint.config.js
+++ mt-plugin-content-field-type-email-react/eslint.config.js
@@ -6,4 +6,11 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
 ]);
